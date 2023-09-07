61ae044288f153ceb773e8c535e666c203f27d3d
RahulGupta20 <40941183+RahulGupta20@users.noreply.github.com>
2023-09-07 17:08:55 +0000

    conversion operation studied

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+// let score = false;
+// let score = false;
+let score = "Rahul";
+// console.log(typeof score)
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false = 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber)
