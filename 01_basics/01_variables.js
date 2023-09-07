4f9186f6a4b1138a2de26add272d893071c8fbd1
RahulGupta20 <40941183+RahulGupta20@users.noreply.github.com>
2023-09-07 10:43:42 +0000

    studied variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID = 12345;
+let accountEmail = 'barkha@gmail.com';
+var accountPassword = '12345';
+accountCity = 'Bettiah';
+let accountState;
+
+// accountID = 2342;
+console.table([accountID, accountEmail, accountPassword, accountState]);
