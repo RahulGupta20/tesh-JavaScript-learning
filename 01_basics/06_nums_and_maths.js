fd7fd88a193b40056d44d26905342ddcd0eff6fa
RahulGupta20 <40941183+RahulGupta20@users.noreply.github.com>
2023-09-14 16:02:41 +0000

    math and num

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,44 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber =  123.7947;
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000;
+/*
+console.log(hundreds.toLocaleString()); // result 1,000,000
+console.log(hundreds.toLocaleString('en-IN ')); // result 10,00,000
+  */
+
+
+/*=========================================================*/
+// Maths
+/*=========================================================*/
+
+/* 
+console.log(Math);
+console.log(Math.abs(-4)); // result 4
+console.log(Math.round(4.6)); // result 5
+console.log(Math.ceil(4.2)); // result 5
+console.log(Math.floor(4.9)); // result 4
+
+console.log(Math.PI);
+console.log(Math.SQRT2);
+*/
+
+// console.log((Math.random() * 20) + 1);
+// console.log(Math.trunc((Math.random() * 20) + 1));
+
+const min = 10;
+const max = 20;
+
+// console.log(Math.random() * (max - min + 1))
+// console.log(Math.random() * (max - min + 1) + min);
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
