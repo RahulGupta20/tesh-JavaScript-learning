fdf45b3ab132dfeda66cdcb9a9a65c4cd34b6ace
RahulGupta20 <40941183+RahulGupta20@users.noreply.github.com>
2023-09-18 16:36:37 +0000

    studing array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,28 @@
+// arrays
+
+const myArr = [0, 1, 2, 3, 4, 5];
+
+
+//push - pop
+/* 
+myArr.push(6); // add an array element at the last of arrray
+myArr.push(7); // add an array element at the last of arrray
+myArr.pop();  // remove an array element at the last of arrray
+console.log(myArr);
+*/
+
+// unshift - shift
+/* 
+myArr.unshift(9) //add an array element at the begining of array
+myArr.shift() //remove an array element at the begining of array
+console.log(myArr);
+*/
+
+// includes
+// console.log(myArr.includes(9)); // includes check if array includes 9
+
+// indexof
+console.log(myArr.indexOf(3)) // check 3 is the which position of the array
+console.log(my)
+
+console.log(myArr);
