2f9571840d5c19aee36e8b06454ace689151b936
RahulGupta20 <40941183+RahulGupta20@users.noreply.github.com>
2023-09-20 15:30:50 +0000

    studied Objects

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -9,7 +9,7 @@ const mySym = Symbol("Key1")
 const jsUser = {
     name: "Hitesh",
     "full name": "Hitesh Chaodhary",
-    mySym: "myKey1",
+    [mySym]: "myKey1",
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -17,7 +17,25 @@ const jsUser = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(jsUser.email);
-console.log(jsUser["email"]); // arko tarika
-console.log(jsUser["full name"]); // arko tarika
-console.log(typeof jsUser.mySym);
+// console.log(jsUser.email);
+// console.log(jsUser["email"]); // arko tarika
+// console.log(jsUser["full name"]); // arko tarika
+// console.log(typeof jsUser[mySym]);
+
+//override object value
+jsUser.email = "rahul@gmail.com"
+// console.log(jsUser.email)
+// Object.freeze(jsUser)
+// jsUser.email = "sonu@gmail.com"
+// console.log(jsUser)
+
+jsUser.greeting = function() {
+    console.log("Hello JS user")
+}
+
+// console.log(jsUser.greeting())
+
+jsUser.greetingTwo = function() {
+    console.log(`Hello JS User ${this.name}`)
+}
+console.log(jsUser.greetingTwo())
