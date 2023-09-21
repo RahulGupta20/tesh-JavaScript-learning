94620ad9d7a0a4dd4911d8fc3bcee67eccde5cb5
RahulGupta20 <40941183+RahulGupta20@users.noreply.github.com>
2023-09-21 17:07:48 +0000

    objects 2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,24 @@
+// const tinder = new objeect();
+/*
+const tinderUser = {};
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+*/
+
+/*
+// Nested Obeject
+const regularUser = {
+    email: "rahulgupta@gmail.com",
+    fullname: {
+        firstname: "Rahul",
+        lastname: "Gupta"
+    }
+}
+console.log(regularUser.fullname.firstname)
+console.log(`My name is ${regularUser.fullname.firstname} ${regularUser.fullname.lastname}`)
+*/
+
