e25faacf9e0582f4bbda85182189c59095cb3a23
RahulGupta20 <40941183+RahulGupta20@users.noreply.github.com>
2023-10-17 18:53:37 +0000

    learning scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -32,10 +32,41 @@ function one() {
 
     function two() {
         const website = "youtube";
-        console.log(username)
+        // console.log(username)
     }
     // console.log(website);
 
     two()
 }
 one();
+
+/*
+if(true) {
+    const username = "hitesh";
+    
+    if(username === "hitesh") {
+        const website = " youtube";
+        console.log(username + website)
+    }
+    // console.log(website);
+}
+// console.log(username)
+*/
+
+//++++++++++++++++++++++++++ interesting ++++++++++++++
+
+function addone(num) {
+    return num + 1;
+}
+addone(5)
+
+// function addtwo(num) {
+//     return num + 2;
+// }
+// addtwo(3);
+
+const addTwo = function(num) {
+    return num + 2;
+}
+
+addTwo(5)
